3c8f6263a29422c450ee735faac4eec592469edd
Todd <toddchatcher@hotmail.com>
2022-10-17 22:07:59 -0500

    First Chp 10 Commit

---
+++ Chp10/UseDinnerPartyWithConstructor.java
@@ -0,0 +1,10 @@
+
+
+public class UseDinnerPartyWithConstructor
+{
+    public static void main(String[] args)
+    {
+        DinnerPartyWithConstructor aDinnerParty = 
+            new DinnerPartyWithConstructor();
+    }
+}
